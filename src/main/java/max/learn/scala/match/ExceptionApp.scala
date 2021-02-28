8a2d774ee348b0bd89d73360f187cbec296aa8ab
Chennan <chennancd@gmail.com>
2021-02-28 11:31:05 +0800

    add scala - implicit match, adv function

---
+++ src/main/java/max/learn/scala/match/ExceptionApp.scala
@@ -0,0 +1,14 @@
+package max.learn.scala.`match`
+
+object ExceptionApp extends App {
+
+  try{
+    val i = 10/0
+    println(i)
+  } catch {
+    case e:ArithmeticException => println("catch exception: 除数不能为0..")
+    case e:Exception => println(e.getMessage)
+  } finally {
+    // something
+  }
+}
